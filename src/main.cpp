24931a89e7e0462cf0e42aa31c34059f236905d6
Edvin <edvinnadj@gmail.com>
2020-11-01 20:39:54 +0100

    Changed BT pins, new command structure from Android app

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,40 @@ void checkForNewCommands();
 void resetStrip();
 void colorWipe();
 void rainbow(byte wait);
-void rainbowCycle(byte wait);
+void rainbowCycle();
 void theaterChase(uint32_t c, byte wait);
 void theaterChaseRainbow(uint32_t wait);
 void applySelectedMode();
-unsigned int parsePrefixedInts(char* receivedChars, const char* prefix);
 uint32_t Wheel(byte WheelPos);
+uint32_t color_wheel(uint8_t pos);
+
+unsigned int hexToDec(String hexString);
+void getArgsof(String message);
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1:
 #define LED_PIN 6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 45
+#define LED_COUNT 75
 
 // Operation Modes
-#define COLORWIPE 1
-#define RAINBOW 2
+// #define COLORWIPE 1
+// #define RAINBOW 2
+
+
+SoftwareSerial BTserial(3, 2); // RX | TX
+// HC05 TX vers Arduino pin A4 RX.
+// HC05 RX vers Arduino pin A3 TX avec diviseur de tension.
+// HC05 STATE pin vers Arduino pin A5.
+
+// BTconnected = false lorsque pas connecte
+boolean BTconnected = false;
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRBW);
 
-const byte numChars = 8;
 boolean newData = false;
-char receivedChars[numChars];   // an array to store the received data
 uint32_t selectedColor = strip.Color(255, 2, 2);
 
 
@@ -39,15 +49,49 @@ uint32_t selectedColor = strip.Color(255, 2, 2);
 unsigned long previousMillis = 0; // will store last time LED was updated
 byte lastLitPixel = 0;
 
+
+
+
+enum MODE { SET_MODE, HOLD, AUTO, COLORWIPE, OFF, TV, CUSTOM, SETCOLOR, SETSPEED, BRIGHTNESS, WIPE, RAINBOW, RAINBOWCYCLE, THEATERCHASE, TWINKLERANDOM, THEATERCHASERAINBOW};
+bool exit_func = false;      // Global helper variable to get out of the color modes when mode changes
+
 // Init data
-byte brightness = 92;
+int brightness = 92;
+// unsigned int interval = 40;
 byte selectedMode = COLORWIPE;
-unsigned int interval = 40;
+
+// ***************************************************************************
+// DEFINE: Default properties
+// ***************************************************************************
+unsigned int ws2812fx_speed = 196;   // Global variable for storing the delay between color changes --> smaller == faster
+
+
+// ***************************************************************************
+// DEFINE: Datatype LED STATE
+// ***************************************************************************
+struct ledstate             // Data structure to store a state of a single led
+{
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t white;
+};
+typedef struct ledstate LEDState;     // Define the datatype LEDState
+
+
+// ***************************************************************************
+// DEFINE: LED STATE for entire LED-Strip
+// *************************************************************************** 
+LEDState ledstates[LED_COUNT];          // Get an array of led states to store the state of the whole strip
+LEDState main_color = { 0, 255, 0, 0};  // Store the "main color" of the strip used in single color modes
+
 
 void setup() {
   initStrip();
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
+  //comm with HC05
+  BTserial.begin(9600);
 }
 
 void loop() {
@@ -73,95 +117,121 @@ void applySelectedMode() {
       case COLORWIPE:
         colorWipe();
         break;
-      case RAINBOW: 
-        rainbowCycle(interval);
+      case RAINBOW:
+        rainbowCycle();
         break;
       default:
         resetStrip();
+        selectedMode = COLORWIPE;
         break;
     }
 }
 
 void checkForNewCommands() {
-  static byte ndx = 0;
-  char endMarker = '\n';
-  char rc;
-
-  while (Serial.available() > 0 && newData == false) {
-    rc = Serial.read();
-
-    if (rc != endMarker) {
-      receivedChars[ndx] = rc;
-      ndx++;
-      if (ndx >= numChars) {
-        ndx = numChars - 1;
-      }
-    }
-    else {
-      receivedChars[ndx] = '\0'; // terminate the string
-      ndx = 0;
-      newData = true;
-    }
+  String received = "";
+  while (BTserial.available() > 0 && newData == false) {
+    received = BTserial.readString();
+    newData = true;
   }
 
   if (newData) {
-    Serial.print("This just in ... ");
-    Serial.println(receivedChars);
+    getArgsof(received);
 
-    switch (receivedChars[0]) {
-      case 'G': {
-        // resetStrip();
-        selectedColor = strip.Color(0, 255, 0);
-        break;
-      }
-      case 'R': {
-        // resetStrip();
-        selectedColor = strip.Color(255, 0, 0);
-        break;
-      }
-      case 'B': {
-        // resetStrip();
-        selectedColor = strip.Color(0, 0, 255);
-        break;
-      }
-      case 'Y': {
-        // resetStrip();
-        selectedColor = strip.Color(255, 255, 0);
-        break;
-      }
-      case 'i': {
-        char prefix[] = "i";
-        interval = parsePrefixedInts(receivedChars, prefix);
-        break;
-      }
-      case 'm': {
-        char prefix[] = "m";
-        resetStrip();
-        selectedMode = parsePrefixedInts(receivedChars, prefix);
-        break;
-      }
-      case 'b': {
-        char prefix[] = "b";
-        if (brightness == 0) {
-          resetStrip();
-        }
-        brightness = parsePrefixedInts(receivedChars, prefix);
-        strip.setBrightness(brightness);
-        strip.show();
-        break;
-      }
-      default: {
-        selectedColor = strip.Color(0, 0, 0, 255); // White
-      }
-    }
+    // apply new data
+    selectedColor = strip.Color(main_color.red, main_color.green, main_color.blue, main_color.white);
+    strip.setBrightness(brightness);
     newData = false;
   }
 }
 
-unsigned int parsePrefixedInts(char* receivedChars, const char * prefix) {
-  char * strtokIndx = strtok(receivedChars, prefix);
-  return atoi(strtokIndx); 
+
+unsigned int hexToDec(String hexString) {
+    unsigned int decValue = 0;
+    int nextInt;
+    for (unsigned int i = 0; i < hexString.length(); i++) {
+        nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57) nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70) nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102) nextInt = map(nextInt, 97, 102, 10, 15);
+        nextInt = constrain(nextInt, 0, 15);
+        decValue = (decValue * 16) + nextInt;
+      }
+    return decValue;
 }
+
+
+
+
+void getArgsof(String message){
+
+             // validate form, massage is in format [00:0000FA00:3D:31]
+            if (!(message.length() == 19 && (message.startsWith("["))&&(message.endsWith("]")))){
+                  Serial.println("[ERROR]: Form Validation failed!");
+                  // gradually slow down animations, to allow message parsing
+                  ws2812fx_speed += 100;
+                  // notify the app that the last command wasn't applied and ask for a resend
+                  BTserial.write("X");
+                  return;
+            }
+            Serial.println(message);
+            
+            String msg = message;
+            // remove Frame
+            msg = msg.substring(1,message.indexOf("]"));
+
+                  int  lmode  =  hexToDec(msg.substring(0,2));  
+                  selectedMode = lmode;
+                  String wrgb = msg.substring(3,11);
+                
+                  uint8_t w  = hexToDec(wrgb.substring(0,2));      main_color.white   = w;
+                  uint8_t r  = hexToDec(wrgb.substring(2,4));      main_color.red     = r;
+                  uint8_t g  = hexToDec(wrgb.substring(4,6));      main_color.green   = g;
+                  uint8_t b  = hexToDec(wrgb.substring(6));        main_color.blue    = b;
+                
+                  int br  = hexToDec(msg.substring(12,14));    brightness         = br;
+                  int lspeed  = hexToDec(msg.substring(15)); 
+                  
+                  ws2812fx_speed = constrain(lspeed, 1, 255);
+                  main_color.white = constrain(main_color.white, 0, 255);
+                  main_color.red   = constrain(main_color.red,   0, 255);
+                  main_color.green = constrain(main_color.green, 0, 255);
+                  main_color.blue  = constrain(main_color.blue,  0, 255);
+            
+              Serial.print("Mode: ");
+              Serial.print(selectedMode);
+              Serial.print(", Color: ");
+              Serial.print(main_color.white);
+              Serial.print(", ");
+              Serial.print(main_color.red);
+              Serial.print(", ");
+              Serial.print(main_color.green);
+              Serial.print(", ");
+              Serial.print(main_color.blue);
+              Serial.print(", Speed:");
+              Serial.print(ws2812fx_speed);
+              Serial.print(", Brightness:");
+              Serial.println(brightness);
+            }
+
+
+/*
+ * Put a value 0 to 255 in to get a color value.
+ * The colours are a transition r -> g -> b -> back to r
+ * Inspired by the Adafruit examples.
+ */
+uint32_t color_wheel(uint8_t pos) {
+  pos = 255 - pos;
+  if(pos < 85) {
+    return ((uint32_t)(255 - pos * 3) << 16) | ((uint32_t)(0) << 8) | (pos * 3);
+  } else if(pos < 170) {
+    pos -= 85;
+    return ((uint32_t)(0) << 16) | ((uint32_t)(pos * 3) << 8) | (255 - pos * 3);
+  } else {
+    pos -= 170;
+    return ((uint32_t)(pos * 3) << 16) | ((uint32_t)(255 - pos * 3) << 8) | (0);
+  }
+}
+
 
 // LED related functions
 
@@ -174,7 +244,7 @@ void colorWipe() {
   // the interval at which you want to blink the LED.
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= ws2812fx_speed) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
     strip.setPixelColor(lastLitPixel, selectedColor);
@@ -198,13 +268,13 @@ void rainbow(byte wait) {
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(byte wait) {
+void rainbowCycle() {
   for (uint16_t j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
     for (uint16_t i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
-    delay(wait);
+    delay(ws2812fx_speed);
   }
 }
 
